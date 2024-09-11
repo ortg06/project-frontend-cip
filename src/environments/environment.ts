574fa1e1065065f8bc4facd9b93e3208f354d5c8
Oscar Turish <ortg_10@hotmail.com>
2024-09-11 14:35:14 -0600

    cambio de url de environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
-export const environment = {
+/*export const environment = {
   production: false,
   url: 'http://localhost:8080'
+};*/
+
+export const environment = {
+  production: false,
+  url: 'http://ec2-18-118-158-16.us-east-2.compute.amazonaws.com:8080'
 };
