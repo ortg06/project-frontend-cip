70dbffc7cd1ce7a50606eb952e36482fb8eae94a
Oscar Turish <ortg_10@hotmail.com>
2024-09-17 11:08:06 -0600

    cambio a https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  url: 'http://ec2-18-118-158-16.us-east-2.compute.amazonaws.com:8080'
+  url: 'https://ec2-18-118-158-16.us-east-2.compute.amazonaws.com:8080'
 };
