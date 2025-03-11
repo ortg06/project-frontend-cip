3be3e419534a941a194d6526c5e895a847b1e849
Oscar Turish <ortg_10@hotmail.com>
2025-03-10 23:20:51 -0600

    fix aws url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  url: 'http://ec2-18-224-40-189.us-east-2.compute.amazonaws.com:8080'
+  url: 'https://ec2-18-224-40-189.us-east-2.compute.amazonaws.com:8080'
 };
